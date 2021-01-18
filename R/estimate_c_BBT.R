0b7f7039fe413eb3df442d458bcb2d1d8340414b
Shunsuke Matsuno <44533758+ShunsukeMatsuno@users.noreply.github.com>
2021-01-18 20:41:36 +0900

    Deleted p_hat since the are added before

--- R/estimate_c_BBT.R
+++ R/estimate_c_BBT.R
@@ -1,8 +1,8 @@
 estimate_c_BBT <- function(df){
   df_cost <- df %>% 
     group_by(firm) %>% 
-    mutate(p_hat = mean(!is.na(disclosure))) %>% 
-    mutate(c_BBT = qnorm(1 - p_hat) + dnorm(qnorm(1 - p_hat)) / (1 - p_hat))
+    mutate(c_BBT = qnorm(1 - p_hat) + dnorm(qnorm(1 - p_hat)) / (1 - p_hat)) %>% 
+    ungroup()
   
   c_BBT <- df_cost %>% pull(c_BBT) 
   
--- R/estimate_c_NP.R
+++ R/estimate_c_NP.R
@@ -1,10 +1,10 @@
 estimate_c_NP <- function(df){
   df_cost <- df %>% 
     group_by(firm) %>% 
-    mutate(p_hat = mean(!is.na(disclosure)),
-           tau_hat = min(disclosure, na.rm = TRUE),
+    mutate(tau_hat = min(disclosure, na.rm = TRUE),
            m_hat = mean(disclosure, na.rm = TRUE)) %>% 
-    mutate(c_NP = max(0, tau_hat + p_hat/(1 - p_hat) * m_hat))
+    mutate(c_NP = max(0, tau_hat + p_hat/(1 - p_hat) * m_hat)) %>% 
+    ungroup()
   
   c_NP <- df_cost %>% pull(c_NP) 
   
