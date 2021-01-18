2f0592b8b9ec0046d04d1e0210c41e9d18486b9c
Shunsuke Matsuno <44533758+ShunsukeMatsuno@users.noreply.github.com>
2021-01-18 20:43:45 +0900

    Wrote separete functions for (i) simulate data and (ii) estiamte cost

--- R/bias_error_plot_distribution.R
+++ R/bias_error_plot_distribution.R
@@ -5,61 +5,17 @@ bias_error_plot_distribution <- function(dist, num_firms, periods){
   #         3 : exponential dist. with rate = 1
   #         4 : t-dist with df 10
 
-  # simulation primitives
-  c_vec <- seq(0.4, 2.1, by = .1)
-  
-  # raw data
-  df_firm <- tibble(
-    firm = rep(1:(num_firms * length(c_vec)), each = periods),
-    time = rep(1:periods, times = num_firms * length(c_vec)),
-    c    = rep(c_vec, each = num_firms * periods),
-    x = case_when(
-      dist == 1 ~ rnorm(num_firms * periods * length(c_vec)),
-      dist == 2 ~ runif(num_firms * periods * length(c_vec), -3, 3),
-      dist == 3 ~ rexp(num_firms * periods * length(c_vec)),
-      dist == 4 ~ rt(num_firms * periods * length(c_vec), 5)
-    )) 
-  
-  # tau
-  df_tau <- df_firm %>% 
-    group_by(firm) %>% 
-    summarise(tau = compute_threshold(x, c),
-              .groups = "drop")
-  
-  # disclosure
-  df_firm <-left_join(df_firm, df_tau, by = "firm") %>% 
-    mutate(disclosure = if_else(x >= tau, x, NA_real_)) %>% 
-    group_by(firm) %>% 
-    mutate(remain_firm = if_else(
-      sum(!is.na(disclosure)) == 0 | sum(is.na(disclosure)) == 0,
-      FALSE,
-      TRUE)) %>% 
-    ungroup() %>% 
-    filter(remain_firm) %>% 
-    select(-remain_firm)
+  # simulate data
+  df_firm <- simulate_data(dist, num_firms, periods)
   
   # cost
-  df_cost <- df_firm %>% 
-    mutate(c_NP  = estimate_c_NP(.),
-           c_BBT = estimate_c_BBT(.)) %>% 
-    group_by(firm) %>% 
-    summarise(across(starts_with("c"), .fns = first),
-              .groups = "drop") %>% 
-    group_by(c) %>% 
-    summarise(bias_NP   = mean(c_NP - c, na.rm = TRUE),
-              bias_BBT  = mean(c_BBT - c, na.rm = TRUE),
-              error_NP  = mean(abs(c_NP - c), na.rm = TRUE),
-              error_BBT = mean(abs(c_BBT - c), na.rm = TRUE),
-              .groups   = "drop")
+  df_cost <- compute_cost_sum(df_firm)
   
   # bias
-  df_cost_plot_bias <- df_cost %>% 
-    group_by(c) %>% 
-    summarise(across(bias_NP:bias_BBT, mean),
-              .groups = "drop") %>% 
-    pivot_longer(-c, names_to = 'type', values_to = 'bias')
-  
-  g1 <- ggplot(df_cost_plot_bias, aes(x = c, y = bias, color = type)) +
+  g1 <- df_cost %>% 
+    select(c, starts_with("bias")) %>% 
+    pivot_longer(-c, names_to = 'type', values_to = 'bias') %>% 
+    ggplot(aes(x = c, y = bias, color = type)) +
     geom_line(aes(linetype = type), size = 1,
               show.legend = FALSE) +
     geom_point(aes(colour = type),
@@ -68,13 +24,10 @@ bias_error_plot_distribution <- function(dist, num_firms, periods){
     theme_bw() 
   
   # error
-  df_cost_plot_error <- df_cost %>% 
-    group_by(c) %>% 
-    summarise(across(error_NP:error_BBT, mean),
-              .groups = "drop") %>% 
-    pivot_longer(-c, names_to = 'type', values_to = 'error')
-  
-  g2 <- ggplot(df_cost_plot_error, aes(x = c, y = error, color = type)) +
+  g2 <- df_cost %>% 
+    select(c, starts_with("error")) %>% 
+    pivot_longer(-c, names_to = 'type', values_to = 'error') %>% 
+    ggplot(aes(x = c, y = error, color = type)) +
     geom_line(aes(linetype = type), size = 1) +
     geom_point(aes(colour = type),
                show.legend = FALSE) +
